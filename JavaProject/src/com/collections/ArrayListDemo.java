dd0df96cffcf0065f98ecb64085f85b4eaff53ab
nihalmujawar <nihalmujawar1019@gmail.com>
2022-10-14 16:48:11 +0530

    first commit

---
+++ JavaProject/src/com/collections/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list=new ArrayList<String>();
+		
+		list.add("Nihal");
+		list.add("Sujit");
+		list.add("Pooja");
+		list.add("Swarali");
+		
+		System.out.println(list);
+
+	}
+
+}
